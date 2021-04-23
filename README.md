d11cb38e984bf75aa0e83a87f1cf5e6ec5a55670
hcw <zh@hcworld.xyz>
2021-04-23 11:31:37 +0800

    feat:创建初始项目结构

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SpringBoot-Shiro-JWT-Scaffolding
-基于SpringBoot-Shiro-JWT-Restful风格的脚手架
+
+## 介绍
+这是一个基于SpringBoot-Shiro-JWT-Restful风格封装的脚手架模板
+
+##项目结构
+
+
+
+## 更新内容
+1.创建初始结构--2021年4月23日
---
+++ src/main/java/xyz/hcworld/scaffolding/ScaffoldingApplication.java
@@ -0,0 +1,23 @@
+package xyz.hcworld.scaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ * @ClassName: ScaffoldingApplication
+ * @Author: 张红尘
+ * @Date: 2021-04-23
+ * @Version： 1.0
+ */
+@SpringBootApplication
+public class ScaffoldingApplication {
+    /**
+     * 启动方法
+     * @param args 启动参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldingApplication.class, args);
+    }
+
+}
